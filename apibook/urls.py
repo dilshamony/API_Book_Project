797facb7ddda19f03fc9f50a0c63029aa58ce82f
Dilsha <dilsham998@gmail.com>
2021-05-28 11:38:19 +0530

    modal serializers-class based,mixin classes

--- apibook/urls.py
+++ apibook/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import book_list,book_details
+from .views import book_list,book_details,\
+    BookListView,BookDetailView,\
+    BookMixinView,BookDetailMixinView
 
 urlpatterns = [
     path("books",book_list,name="books"),
-    path("books/<int:id>",book_details,name="bookdetails")
+    path("books/<int:id>",book_details,name="bookdetails"),
+    path("cbooks",BookListView.as_view(),name="cbooks"),
+    path("cbooks/<int:id>",BookDetailView.as_view(),name="cdetail"),
+    path("mbooks",BookMixinView.as_view(),name="mbooks"),
+    path("mbooks/<int:pk>",BookDetailMixinView.as_view(),name="mdetail")
 ]
--- apibook/views.py
+++ apibook/views.py
@@ -8,6 +8,9 @@ from django.http import JsonResponse
 #___________________________________
 
 from .serializers import BookModelSerializer
+from rest_framework.views import APIView
+#___________________________________
+from rest_framework import mixins, generics
 
 
 
@@ -58,3 +61,67 @@ def book_details(request,id):
 
 #                                      CLASS BASED VIEWS
 #=======================================================================================================================
+#LIST
+class BookListView(APIView):
+    def get(self,request):
+        books=Book.objects.all()
+        serializer=BookModelSerializer(books,many=True)
+        return JsonResponse(serializer.data,safe=False)
+#CREATE
+    def post(self,request):
+        serializer=BookModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        else:
+            return JsonResponse(serializer.data, status=400)
+#DETAILS
+class BookDetailView(APIView):
+    def get_object(self,id):
+        return Book.objects.get(id=id)
+    def get(self,request,id):
+        book=self.get_object(id)
+        serializer=BookModelSerializer(book)
+        return JsonResponse(serializer.data, status=201)
+    def put(self,request,id):
+        book = self.get_object(id)
+        serializer = BookModelSerializer(book,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        else:
+            return JsonResponse(serializer.data, status=400)
+    def delete(self,request,id):
+        book = self.get_object(id)
+        book.delete()
+        return JsonResponse({"Message":"Deleted"})
+
+
+#                                     MIXIN CLASSES
+#=======================================================================================================================
+#LIST
+class BookMixinView(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    generics.GenericAPIView):
+    queryset=Book.objects.all()
+    serializer_class = BookModelSerializer
+    def get(self,request,*args,**kwargs):
+        return self.list( request, *args, **kwargs)
+#CREATE
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+#DETAIL
+class BookDetailMixinView(generics.GenericAPIView,
+                          mixins.RetrieveModelMixin,
+                          mixins.UpdateModelMixin,
+                          mixins.DestroyModelMixin):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request,*args,**kwargs)
+#UPDATE
+    def put(self, request, *args, **kwargs):
+        return self.update(request,*args,**kwargs)
+#DELETE
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request,*args,**kwargs)
